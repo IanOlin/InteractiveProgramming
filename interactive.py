bf694b19e5778049b5c4d80cb8129341d1fd2cf9
Ian Paul <ian.paul@students.olin.edu>
2016-03-03 15:12:33 -0500

    added a pic and made it bg

--- interactive.py
+++ interactive.py
@@ -1,6 +1,6 @@
 #vim : tabstop=8 expandtab shiftwidth=4 softtabstop=4
 import pygame
-from pygame.locals import QUIT, KEYDOWN
+from pygame.locals import * 
 import time
 from random import choice
 
@@ -10,14 +10,16 @@ class PyGameTestView(object):
     """ This is a test view for my game"""
     def __init__(self, model, size):
         self.model = model
-        self.screen = pygame.display.set_mode(size)
+        self.screen = pygame.display.set_mode(size, HWSURFACE|DOUBLEBUF|RESIZABLE)
 
     def draw(self):
         """ Draw the game to the pygame window """
         self.screen.fill(pygame.Color('black')) #want to replace with image
-        # background = pygame.image.load('background.bmp')
-        # in game loop: gameDisplay.blit(bg, (0,0)) bg size=screensize
-
+        background = pygame.image.load('room2.png')
+        #self.screen.blit(pygame.transform.scale(background, (640,480)) (0,0))
+        #pygame.display.flip()
+        # see pygame window resizing to find docs for this. current broked
+        self.screen.blit(background, (0,0))
         #draw the rest of the game
         pygame.display.update()
 
@@ -50,7 +52,7 @@ class PyGameTestController(object):
             pass
 if __name__ == '__main__':
     pygame.init()
-    size = (640,480)
+    size = (320,240)
 
     model = PyGameTestModel(size[0], size[1])
     view = PyGameTestView(model, size)
