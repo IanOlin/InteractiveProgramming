99303ca9bbb0f7843e799e333ed62ed5c662211f
Halliax <tmsheneman@gmail.com>
2016-03-08 22:17:50 -0500

    collision rectangle for the character now scaled to feet and working correctly. Drew a map, map doesnt yet map the map rectangles to the map. Restructured code to put all the animation and movement data in the CharacterSprite class. Added attributes to docstrings.

--- interactive.py
+++ interactive.py
@@ -5,37 +5,63 @@ import time
 from random import choice
 
 #TODO split the model view and controller into different files maybe?
-#docs:
+# docs:
 # pygame.org/project-Rect+collision+Reponse-1061-.html
-#that link was hand typed so fuck off
+# that link was hand typed so fuck off
+# the screen is a 20x15 grid of 16x16 pixel tiles
+# at least in 320 I guess it's 32x32 in 640
 class GameView(object):
-    """this view handles displaying most o the things"""
+    """this view handles displaying most of the things.
+
+    attributes: model, screen"""
     def __init__(self, model, size):
         self.model = model
         self.screen = pygame.display.set_mode(size, HWSURFACE|DOUBLEBUF|RESIZABLE)
-        pygame.mixer.music.load('sounds/TheDarkLake.mp3') #this music might or mightnot play
+        # apparently HWSURFACE is a pain in the ass to work with, and we're
+        # probably getting a SWSURFACE anyway
+        pygame.mixer.music.load('sounds/TheDarkLake.mp3') #this music plays
         pygame.mixer.music.play(-1)
-        
-        
+
+
     def draw(self):
-        """ Draw the game to the pygame window """
-        background = pygame.image.load('images/room2.png')
+        """ Draw the game to the pygame window"""
+        background = pygame.image.load('images/room2.png').convert()
         scaled_bg = pygame.transform.scale(background, size)
         self.screen.blit(scaled_bg, (0,0))
         char = self.model.char.image
         self.screen.blit(char, (self.model.char.x,self.model.char.y))
+        pygame.draw.rect(self.screen, (255, 200, 0), self.model.char.rect)
+        # draws a yellow box where the rect is
         pygame.display.flip()
 
 class GameModel(object):
-    """This is a test model for my game"""
+    """This is a test model for my game
+
+    attributes: height, width, char, game_map"""
     def __init__(self, width, height):
         self.height = height
         self.width = width
 
         # Define things like brick height and width here for BB
         # Also creates list of bricks, paddle, and ball
-        
+
         self.char = CharacterSprite(0,0)
+
+        self.game_map = ["....................",
+                         "....................",
+                         "....................",
+                         "....................",
+                         "....................",
+                         "......WEWW..........",
+                         "....WWW..WWWWW......",
+                         "....W........W......",
+                         "....W........W......",
+                         "....W..........W....",
+                         "....W..........W....",
+                         "....W..........W....",
+                         "....WWWWWEEWWWWW....",
+                         "....................",
+                         "...................."]
 
 
     def update(self, control=0):
@@ -43,7 +69,9 @@ class GameModel(object):
         self.char.update(control)
 
 class SpriteSheet(object):
-    """Class used to grab images out of a sprite sheet."""
+    """Class used to grab images out of a sprite sheet.
+
+    attributes: sprite_sheet"""
     def __init__(self, file_name= 'images/smallspritesheet.png'):
         self.sprite_sheet = pygame.image.load(file_name)
     def get_image(self, x, y, width, height):
@@ -51,23 +79,29 @@ class SpriteSheet(object):
         image = pygame.Surface([width, height])
 
         image.blit(self.sprite_sheet, (0,0), (x, y, width, height))
-        
+
         image.set_colorkey((0,0,0))
 
         return image
+
 class CharacterSprite(pygame.sprite.Sprite):
-    """Sprite representation of the character"""
+    """Sprite representation of the character.
+
+    attributes: x, y, walking_frames_l, walking_frames_r, walking_frames_u,
+    walking_frames_d, step_size, direction, image, rect"""
     def __init__(self, x_coor, y_coor):
         self.x = x_coor
         self.y = y_coor
+        self.step_size = 5
 
         self.walking_frames_l = []
         self.walking_frames_r = []
         self.walking_frames_u = []
         self.walking_frames_d = []
+        self.effect = pygame.mixer.Sound('sounds/step.wav')
 
         self.direction = 'd'
-        #list of colisions would go here 
+        #list of colisions would go here  #(maybe)
         sprite_sheet = SpriteSheet('images/smallspritesheet.png')
         for i in range(0,109, 36):
             image = sprite_sheet.get_image(i,0,36, 60)
@@ -81,62 +115,85 @@ class CharacterSprite(pygame.sprite.Sprite):
         for i in range(0,109, 36):
             image = sprite_sheet.get_image(i,185,36, 70)
             self.walking_frames_u.append(image)
-       
+
         self.walking_frames_d[3] = self.walking_frames_d[1]
         self.walking_frames_l[3] = (self.walking_frames_l[1])
         self.walking_frames_u[3] = (self.walking_frames_u[1])
         self.walking_frames_r[3] = (self.walking_frames_r[1])
         self.image = self.walking_frames_d[1]
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect().inflate(-4,-32)
+        self.rect.move_ip(0,16)
 
+    def move_left(self, li):
+        self.x -= self.step_size
+        self.rect.move_ip(-self.step_size, 0)
+        self.image = self.walking_frames_l[li]
+        self.effect.play()
+
+    def move_right(self, ri):
+        self.x += self.step_size
+        self.rect.move_ip(self.step_size, 0)
+        self.image = self.walking_frames_r[ri]
+        self.effect.play()
+
+    def move_up(self, ui):
+        self.y -= self.step_size
+        self.rect.move_ip(0, -self.step_size)
+        self.image = self.walking_frames_u[ui]
+        self.effect.play()
+
+    def move_down(self, di):
+        self.y += self.step_size
+        self.rect.move_ip(0, self.step_size)
+        self.image = self.walking_frames_d[di]
+        self.effect.play()
 
 class GameController(object):
-    """This is the controller for the game. It contains all the animation data """
+    """This is the controller for the game. It contains a counter for each
+    direction that controls animation state.
+
+    attributes: model, li, di, ri, ui"""
     def __init__(self, model):
-        """ initializes the model and all of the image banks for the character"""
+        """ initializes the model and counters"""
         self.model = model
         self.li = 0
-        self.di =0
-        self.ri =0
-        self.ui =0
-        self.effect = pygame.mixer.Sound('sounds/step.wav')
+        self.di = 0
+        self.ri = 0
+        self.ui = 0
 
     def update(self):
-        """ Updates the game state based on keypresses. Also animates walking right now"""
+        """ Updates the game state based on keypresses.
+        Also animates walking ALL DIRECTIONS now"""
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_LEFT]:
-            self.li = (self.li+1)%4
-            self.model.char.x -= 5
-            self.model.char.image = self.model.char.walking_frames_l[self.li]
-            self.effect.play()
+            self.li = (self.li + 1) % 4
+            self.model.char.move_left(self.li)
+
         if pressed[pygame.K_RIGHT]:
-            self.ri = (self.ri+1)%4
-            self.model.char.x += 5
-            self.model.char.image = self.model.char.walking_frames_r[self.ri]
-            self.effect.play()
+            self.ri = (self.ri + 1) % 4
+            self.model.char.move_right(self.ri)
+
         if pressed[pygame.K_UP]:
-            self.ui = (self.ui + 1)%4
-            self.model.char.y -= 5
-            self.model.char.image = self.model.char.walking_frames_u[self.ui]
-            self.effect.play()
+            self.ui = (self.ui + 1) % 4
+            self.model.char.move_up(self.ui)
+
         if pressed[pygame.K_DOWN]:
-            self.di = (self.di +1)%4
-            self.model.char.image = self.model.char.walking_frames_d[self.di]
-            self.model.char.y += 5 
-            self.effect.play()
+            self.di = (self.di + 1) % 4
+            self.model.char.move_down(self.di)
+
 if __name__ == '__main__':
     pygame.init()
-    #size = (VideoInfo.current_w, VideoInfo.current_h)#sets the game to fill creen
-    #That works, but it doesn't geneate in the right aspect ratio
+    #size = (VideoInfo.current_w, VideoInfo.current_h)#sets the game to fill screen
+    #That works, but it doesn't generate in the right aspect ratio
     size = (640, 480) # useful
-    # size = (320,240)  #'native'
+    # size = (320, 240)  # 'native'
     model = GameModel(size[0], size[1])
     view = GameView(model, size)
     controller = GameController(model)
     running = True
     while running:
         for event in pygame.event.get():
-            if event.type ==QUIT:
+            if event.type == QUIT:
                 running = False
         model.update()
         controller.update()
