e9bfddb6ed9d45bc76c6ccc18ebaaadada95d3a0
Ian Paul <ian.paul@students.olin.edu>
2016-03-09 23:56:18 -0500

    Made a random level string generator at MVP state.
    Can generate a box with between 0 and max exits on the walls and a giant empty space in between.

--- level.py
+++ level.py
@@ -5,28 +5,28 @@ import random
 
 class Level(object):
 	#inherit from spritesheet? probably no, but we will need some sort of visual level as well as the programatic level
-	def __init__(self):
-		pass
+	def __init__(self, x=0):
+            self.x = x
 
-	def random_gen():
+	def random_gen(self):
 		#probably should take size
 		#15 strings of 20 characters
 
 		#generate top wall
 		twall = ''
 		for x in range(15):
-			twall += random.choice('W','W','W','W','X')
+			twall += random.choice(['W','W','W','W','X'])
 
 		bwall = ''
 		for x in range(15):
-			bwall += random.choice('W','W','W','W','X')
+			bwall += random.choice(['W','W','W','W','X'])
 
 		middlewalls = []
 		for x in range(18):
 			wall = ''
-			wall += random.choice('W','W','W','W','X')
+			wall += random.choice(['W','W','W','W','X'])
 			wall += '.................'
-			wall += random.choice('W','W','W','W','X')
+			wall += random.choice(['W','W','W','W','X'])
 			middlewalls.append(wall)
 
 		walls = []
@@ -36,7 +36,7 @@ class Level(object):
 		walls.append(bwall)
 		print walls
 
-Level.random_gen
 
 
-
+level = Level()
+level.random_gen()
